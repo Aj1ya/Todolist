4c8a086922b377429bc2563dba66dc313eeeaf17
Ajaya Maharjan <ajayamaharjan93@gmail.com>
2023-04-10 22:18:12 +1000

    Update: Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["buy food", "cook food", "eat food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=>{
+
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    
+    var day = today.toLocaleDateString("en-US", options);
+   
+    res.render( "list", {kindOfDay: day, newListItems:items} );
+
+});
+
+app.post("/", (req, res)=>{
+    var item = req.body.newItem;
+    items.push( item );
+    res.redirect("/");
+});
+
+app.listen(3000, (req, res)=>{
+    console.log( "app started in 3000" );
+});
