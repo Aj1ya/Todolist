f0b1e824f64f61f12752be35f08c08cfc6e18a12
Ajaya Maharjan <ajayamaharjan93@gmail.com>
2023-04-11 19:26:59 +1000

    Update: Stylesheet and js condition added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Todolist
 This is a to do list app.
+
+
+version 1.1
+
+#Add
+- Added stylesheet to the app.
+
+#Fix
+- Updated the js to use "let" and "const" in place of var,
+- Conditional logic in place to avoid adding empty items to the list.
--- app.js
+++ app.js
@@ -5,31 +5,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["buy food", "cook food", "eat food"];
+let items = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res)=>{
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
     
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
    
     res.render( "list", {kindOfDay: day, newListItems:items} );
 
 });
 
 app.post("/", (req, res)=>{
-    var item = req.body.newItem;
-    items.push( item );
+    let item = req.body.newItem;
+    if( !(item == null || item == "") ){
+        items.push( item );
+    }
     res.redirect("/");
 });
 
