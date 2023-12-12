30f7e088d91853e9f68a14daa321e31ab1e91713
Ajaya Maharjan <ajayamaharjan93@gmail.com>
2023-12-12 21:47:48 +1100

    Add: Delete task function added

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
+// Add this route before your existing app.post("/") route
+app.delete("/delete/:index", (req, res) => {
+    const index = req.params.index;
+
+    // Determine the list based on the request
+    const targetList = req.body.list === "work" ? workItems : items;
+
+    // Check if the index is valid
+    if (index >= 0 && index < targetList.length) {
+        // Remove the item at the specified index
+        targetList.splice(index, 1);
+
+        // Respond with success
+        res.json({ success: true });
+    } else {
+        // Respond with an error if the index is invalid
+        res.json({ success: false, error: "Invalid index" });
+    }
+});
+
 app.get("/", (req, res)=>{
 
     let day = date.getDate();
@@ -25,8 +45,8 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/", (req, res)=>{
-    console.log(req.body.list);
     let item = req.body.newItem;
+    
 
     if( !(item == null || item == "") ){
         if( req.body.list === "work"){
@@ -37,6 +57,8 @@ app.post("/", (req, res)=>{
             res.redirect("/");
         }
     }
+
+
   
 });
 
@@ -51,3 +73,7 @@ app.get("/about", (req, res)=>{
 app.listen(3000, (req, res)=>{
     console.log( "app started in 3000" );
 });
+
+
+
+
